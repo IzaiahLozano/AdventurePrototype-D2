7d67dca30e68eb69707d65480955a99a19102fa7
Izaiah <iilozano@ucsc.edu>
2023-05-22 15:52:30 -0700

    Start of D2

--- game.js
+++ game.js
@@ -1,131 +1,110 @@
-class Demo1 extends AdventureScene {
+class ProductionIntro extends Phaser.scene {
     constructor() {
-        super("demo1", "First Room");
+        super('Production_intro');
     }
 
-    onEnter() {
-
-        let clip = this.add.text(this.w * 0.3, this.w * 0.3, "📎 paperclip")
-            .setFontSize(this.s * 2)
-            .setInteractive()
-            .on('pointerover', () => this.showMessage("Metal, bent."))
-            .on('pointerdown', () => {
-                this.showMessage("No touching!");
-                this.tweens.add({
-                    targets: clip,
-                    x: '+=' + this.s,
-                    repeat: 2,
-                    yoyo: true,
-                    ease: 'Sine.inOut',
-                    duration: 100
-                });
-            });
-
-        let key = this.add.text(this.w * 0.5, this.w * 0.1, "🔑 key")
-            .setFontSize(this.s * 2)
-            .setInteractive()
-            .on('pointerover', () => {
-                this.showMessage("It's a nice key.")
-            })
-            .on('pointerdown', () => {
-                this.showMessage("You pick up the key.");
-                this.gainItem('key');
-                this.tweens.add({
-                    targets: key,
-                    y: `-=${2 * this.s}`,
-                    alpha: { from: 1, to: 0 },
-                    duration: 500,
-                    onComplete: () => key.destroy()
-                });
-            })
-
-        let door = this.add.text(this.w * 0.1, this.w * 0.15, "🚪 locked door")
-            .setFontSize(this.s * 2)
-            .setInteractive()
-            .on('pointerover', () => {
-                if (this.hasItem("key")) {
-                    this.showMessage("You've got the key for this door.");
-                } else {
-                    this.showMessage("It's locked. Can you find a key?");
-                }
-            })
-            .on('pointerdown', () => {
-                if (this.hasItem("key")) {
-                    this.loseItem("key");
-                    this.showMessage("*squeak*");
-                    door.setText("🚪 unlocked door");
-                    this.gotoScene('demo2');
-                }
-            })
-
+    preload(){
+        this.load.path = './assets/';
+        this.load.image('sn', 'Studio.png');
+        this.load.image('star', 'Star.png');
+        //this.load.audio('ptheme', 'intro.mp3');
     }
-}
 
-class Demo2 extends AdventureScene {
-    constructor() {
-        super("demo2", "The second room has a long name (it truly does).");
-    }
-    onEnter() {
-        this.add.text(this.w * 0.3, this.w * 0.4, "just go back")
-            .setFontSize(this.s * 2)
-            .setInteractive()
-            .on('pointerover', () => {
-                this.showMessage("You've got no other choice, really.");
-            })
-            .on('pointerdown', () => {
-                this.gotoScene('demo1');
-            });
-
-        let finish = this.add.text(this.w * 0.6, this.w * 0.2, '(finish the game)')
-            .setInteractive()
-            .on('pointerover', () => {
-                this.showMessage('*giggles*');
-                this.tweens.add({
-                    targets: finish,
-                    x: this.s + (this.h - 2 * this.s) * Math.random(),
-                    y: this.s + (this.h - 2 * this.s) * Math.random(),
-                    ease: 'Sine.inOut',
-                    duration: 500
-                });
-            })
-            .on('pointerdown', () => this.gotoScene('outro'));
-    }
-}
-
-class Intro extends Phaser.Scene {
-    constructor() {
-        super('intro')
-    }
     create() {
-        this.add.text(50,50, "Adventure awaits!").setFontSize(50);
-        this.add.text(50,100, "Click anywhere to begin.").setFontSize(20);
-        this.input.on('pointerdown', () => {
-            this.cameras.main.fade(1000, 0,0,0);
-            this.time.delayedCall(1000, () => this.scene.start('demo1'));
+
+        //create image object 
+        let prod = this.add.image(
+            400,//x
+            250,//y
+            'sn',//studioname
+        )
+        prod.setScale(0.8) //resize
+
+        //create image object 
+        let star1 = this.add.image(
+            0,//x
+            100,//y
+            'star',//imagename
+        )
+        star1.setScale(0.1) //resize
+
+        let star2 = this.add.image(
+            0,//x
+            100,//y
+            'star',//imagename
+        )
+        star2.setScale(0.1) //resize
+
+        // let theme = this.sound.add(
+        //  "ptheme",
+        // );
+        // theme.play();
+
+        this.tweens.add({
+            targets: prod,
+            alpha: {start: 0, to: 1},
+            duration: 7000,
+            //ease: "Sine",
+            //repeat: -1
+         });
+
+
+         this.tweens.add({
+            targets: star1,
+            x:271,
+            y:230,
+            duration: 4000,
+            ease: 'Linear',
+            //repeat: -1,
         });
+
+
+        this.tweens.add({
+            targets: star2,
+            x:544,
+            y:230,
+            duration: 4000,
+            ease: 'Linear',
+            //repeat: -1,
+        });
+
+
+        this.tweens.add({
+            targets: [star1, star2],
+            scale: {start: 0.1, to: 14},
+            delay: 7000,
+            duration: 4000,
+            //ease: "Sine",
+            //repeat: -1
+         });
+
+
+         this.tweens.add({
+            targets: prod,
+            delay: 7000,
+            alpha: {start: 0, to: 1, to : 0},
+            duration: 2000,
+            //ease: "Sine",
+            //repeat: -1
+         });
+
+         this.tweens.add({
+            targets: [star1, star2],
+            delay: 9000,
+            alpha: {from: 1, to: 0},
+            duration: 3000,
+            ease: "Sine",
+            //repeat: -1
+         });
+
+        
+
+        this.time.delayedCall(8000, () => {
+            this.cameras.main.fadeOut(2000, 0,0,0);
+        });
+        //this.input.on('pointerdown', () => theme.stop);
+        this.input.on('pointerdown', () => this.scene.start('Story'));
     }
+
+
 }
-
-class Outro extends Phaser.Scene {
-    constructor() {
-        super('outro');
-    }
-    create() {
-        this.add.text(50, 50, "That's all!").setFontSize(50);
-        this.add.text(50, 100, "Click anywhere to restart.").setFontSize(20);
-        this.input.on('pointerdown', () => this.scene.start('intro'));
-    }
-}
-
-
-const game = new Phaser.Game({
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: 1920,
-        height: 1080
-    },
-    scene: [Intro, Demo1, Demo2, Outro],
-    title: "Adventure Game",
-});
-
