ee10ddd1ae623290622977c38b76779c63a17752
Izaiah <iilozano@ucsc.edu>
2023-05-23 02:52:42 -0700

    Finished intro/main menu

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-class ProductionIntro extends Phaser.Scene {
+class ProductionIntro extends AdventureScene {
     constructor() {
         super('Production_intro');
     }
@@ -7,72 +7,74 @@ class ProductionIntro extends Phaser.Scene {
         this.load.path = './assets/';
         this.load.image('sn', 'Studio.png');
         this.load.image('star', 'Star.png');
-        //this.load.audio('ptheme', 'intro.mp3');
+        this.load.image('preview', 'starting-room.jpg')
+        this.load.image('start', 'playbtn.png')
+        this.load.image('title', 'gtitle.png')
+        this.load.image('light', 'light.png')
+        this.load.script('webfont', 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js');
+        
     }
 
     create() {
 
+        this.cameras.main.setBackgroundColor('#fffff1');
+
         //create image object 
         let prod = this.add.image(
-            400,//x
-            250,//y
+            960,//x
+            540,//y
             'sn',//studioname
         )
-        prod.setScale(0.8) //resize
+        prod.setScale(2) //resize
 
         //create image object 
         let star1 = this.add.image(
             0,//x
-            100,//y
+            0,//y
             'star',//imagename
         )
-        star1.setScale(0.1) //resize
+        star1.setScale(.1) //resize
 
         let star2 = this.add.image(
             0,//x
-            100,//y
+            0,//y
             'star',//imagename
         )
-        star2.setScale(0.1) //resize
-
-        // let theme = this.sound.add(
-        //  "ptheme",
-        // );
-        // theme.play();
+        star2.setScale(.1) //resize
 
         this.tweens.add({
             targets: prod,
             alpha: {start: 0, to: 1},
-            duration: 7000,
-            //ease: "Sine",
-            //repeat: -1
+            duration: 4000,
          });
 
 
          this.tweens.add({
             targets: star1,
-            x:271,
-            y:230,
-            duration: 4000,
+            x:635,
+            y:490,
+            duration: 3500,
             ease: 'Linear',
+            scale: 0.2,
             //repeat: -1,
         });
 
 
         this.tweens.add({
             targets: star2,
-            x:544,
-            y:230,
-            duration: 4000,
+            x:1320,
+            y:490,
+            duration: 3500,
             ease: 'Linear',
+            scale: 0.2,
             //repeat: -1,
         });
 
 
         this.tweens.add({
             targets: [star1, star2],
-            scale: {start: 0.1, to: 14},
-            delay: 7000,
+            scale: {start: 0.3, to: 50},
+            delay: 6000,
             duration: 4000,
             //ease: "Sine",
             //repeat: -1
@@ -81,19 +83,10 @@ class ProductionIntro extends Phaser.Scene {
 
          this.tweens.add({
             targets: prod,
-            delay: 7000,
+            delay: 6000,
             alpha: {start: 0, to: 1, to : 0},
             duration: 2000,
             //ease: "Sine",
-            //repeat: -1
-         });
-
-         this.tweens.add({
-            targets: [star1, star2],
-            delay: 9000,
-            alpha: {from: 1, to: 0},
-            duration: 3000,
-            ease: "Sine",
             //repeat: -1
          });
 
@@ -102,12 +95,150 @@ class ProductionIntro extends Phaser.Scene {
         this.time.delayedCall(8000, () => {
             this.cameras.main.fadeOut(2000, 0,0,0);
         });
-        //this.input.on('pointerdown', () => theme.stop);
-        //this.input.on('pointerdown', () => this.scene.start('Story'));
+
+
+
+
+
+        //MENU PART:
+
+        let mainu = this.add.image(
+            960, 
+            540,
+            "preview",
+         )
+         mainu.setScale(1)
+
+         this.tweens.add({
+            targets: mainu,
+            x:960,
+            y:540,
+            alpha: {start: 0, to: 1},
+            delay: 11000,
+            duration: 1000
+        });
+
+        this.time.delayedCall(11000, () => {
+            this.cameras.main.fadeIn(2000, 0,0,0);
+        });
+
+
+
+        let flicker = this.add.image(
+            920,//x
+            420,//y
+            'light',//studioname
+        )
+        flicker.setScale(1)
+
+        this.tweens.add({
+            // x: 900,
+            // y: 900,
+            targets: flicker,
+            delay: 11000,
+            alpha: {start:0, to: 1},
+            ease: 'Sine.InOut',
+            duration: 3000,
+            repeat: -1,
+            yoyo: true,
+        })
+
+
+
+        let title = this.add.image(
+            850,//x
+            450,//y
+            'title',//studioname
+        )
+        title.setScale(1)
+
+
+        this.tweens.add({
+            // x: 900,
+            // y: 900,
+            targets: title,
+            delay: 13000,
+            alpha: {start:0, to: 1},
+            duration: 3000,
+        })
+
+
+        let play = this.add.image(
+            930,//x
+            580,//y
+            'start',//studioname
+        )
+        play.setScale(0.3)
+
+        this.tweens.add({
+            // x: 900,
+            // y: 900,
+            targets: play,
+            delay: 14000,
+            alpha: {start:0, to: 1},
+            duration: 3000,
+        })
+
+
+
+        play.setInteractive();
+        play.on('pointerover', () => {
+            this.tweens.add({
+                targets: play,
+                scaleX: .5,
+                scaleY: .5,
+                duration: 200
+            });
+        });
+        
+        play.on('pointerout', () => {
+            this.tweens.add({
+                targets: play,
+                scaleX: .3,
+                scaleY: .3,
+                duration: 200
+            });
+        });
+
+
+        play.on('pointerdown', () => {
+            this.cameras.main.fadeOut(1000, 0, 0, 0);
+            this.time.delayedCall(1000, () => this.scene.start('Room1'));
+        });
+
+
     }
 
 
+    
+
+
 }
+
+
+
+class Room1 extends AdventureScene {
+    constructor() {
+        super('beginning');
+    }
+
+    preload(){
+        this.load.path = './assets/';
+        this.load.image('rm', 'starting-room.jpg');
+        this.load.script('webfont', 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js');
+
+    }
+
+
+    create(){
+
+        
+
+
+    }
+
+}
+
 
 const game = new Phaser.Game({
     scale: {
